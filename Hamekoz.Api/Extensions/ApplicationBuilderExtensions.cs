3258c719be9a8ecd8c811daefc10b36b4b2f2173
Rodrigo Pereyra Diaz <rodrigo@pereyradiaz.com.ar>
2025-09-07 02:15:42 -0300

    feat: add hamekoz application builder extensions

---
+++ Hamekoz.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Hamekoz.Api.Extensions;
+
+public static class ApplicationBuilderExtensions
+{
+    public static IApplicationBuilder UseHamekozMiddlewares(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<Middlewares.ExceptionHandlingMiddleware>();
+
+        return app;
+    }
+
+}
