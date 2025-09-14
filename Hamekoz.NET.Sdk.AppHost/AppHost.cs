c785fac116386d53a93d96bfc086d20dabb8b693
Rodrigo Pereyra Diaz <rodrigo@pereyradiaz.com.ar>
2025-09-14 10:33:27 -0300

    chore: add Hamekoz.Auth.Service.Api proyect

--- Hamekoz.NET.Sdk.AppHost/AppHost.cs
+++ Hamekoz.NET.Sdk.AppHost/AppHost.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.Hamekoz_Api_Example>("hamekoz-api-example");
 
+builder.AddProject<Projects.Hamekoz_Auth_Service_Api>("hamekoz-auth-service-api");
+
 builder.Build().Run();
