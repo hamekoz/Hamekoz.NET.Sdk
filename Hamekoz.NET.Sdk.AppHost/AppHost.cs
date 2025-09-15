f9380de62e1413c3e0cda54c9b568d640a195652
Rodrigo Pereyra Diaz <rodrigo@pereyradiaz.com.ar>
2025-09-15 07:44:24 -0300

    refactor: configure sql server in aspire

--- Hamekoz.NET.Sdk.AppHost/AppHost.cs
+++ Hamekoz.NET.Sdk.AppHost/AppHost.cs
@@ -1,7 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var sqlServer = builder.AddSqlServer("sqlserver");
+
 builder.AddProject<Projects.Hamekoz_Api_Example>("hamekoz-api-example");
 
-builder.AddProject<Projects.Hamekoz_Auth_Service_Api>("hamekoz-auth-service-api");
+var hamekozAuthServiceDb = sqlServer.AddDatabase("hamekoz-auth-service-api-db");
+
+builder.AddProject<Projects.Hamekoz_Auth_Service_Api>("hamekoz-auth-service-api")
+    .WithReference(hamekozAuthServiceDb);
 
 builder.Build().Run();
